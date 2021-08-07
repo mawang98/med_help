0de0b88e26094dd4cc8d81b6a22c08658d288a3a
Admin <mawang98@163.com>
2021-08-07 15:01:10 +0800

    202108071501

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ class MainWindow(QtWidgets.QMainWindow):
         size = self.geometry()
         self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
 
+    def ini_set(self):
+        self.ui.pushButton.clicked.connect(self.refresh_list)
+        self.ui.action.clicked.connect()
+
+    def set_comment_win(self):
+        pass
+
     def refresh_list(self):  #更新列表内容
         list_content = []
         try:
@@ -30,6 +37,7 @@ class MainWindow(QtWidgets.QMainWindow):
         if len(b)==0:
             QtWidgets.QMessageBox.warning(self,'检索数据为空','没有检索到数据，请检查数据库')
         else:
+            self.ui.listWidget.clear()
             for i in range(len(b)):
                 list_content.append(b[i][0])
                 self.ui.listWidget.insertItem(i,b[i][0])
